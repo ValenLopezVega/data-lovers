fdc30a8e90caedc4d715bb24320bf35da5031b5c
Jhoana <jhoanazamora2@gmail.com>
2023-03-04 13:40:25 -0500

    Agregar implementaciones de filtrado, ordenado y búsqueda

--- src/data.js
+++ src/data.js
@@ -1,10 +1,12 @@
-// estas funciones son de ejemplo
-
-// export const example = () => {
-
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+export const filterByDirector = (array, directorSelected) => {
+  return array.filter((item) => item.director === directorSelected);
+};
+export const orderAzByTitle = (array) => {
+  return array.sort((a, b) => a.title.localeCompare(b.title));
+};
+export const orderZaByTitle = (array) => {
+  return array.sort((a, b) => b.title.localeCompare(a.title));
+};
+export const searchByTitle = (array, text) => {
+  return array.filter((item) => item.title.toLowerCase().includes(text));
+};
