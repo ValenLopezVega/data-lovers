fda8b4a2d26478d5e0006a46c9cceb1b99520349
MayAlcione <myalcione@gmail.com>
2023-03-06 11:33:02 -0300

    Boton de seleccionar año y productor agregado

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 export const filterByDirector = (array, directorSelected) => {
   return array.filter((item) => item.director === directorSelected);
 };
+export const filterByProducer = (array, producerSelected) => {
+    return array.filter((item) => item.producer === producerSelected);
+  };
 export const orderAzByTitle = (array) => {
   return array.sort((a, b) => a.title.localeCompare(b.title));
 };
@@ -10,3 +13,8 @@ export const orderZaByTitle = (array) => {
 export const searchByTitle = (array, text) => {
   return array.filter((item) => item.title.toLowerCase().includes(text));
 };
+
+export const filterByYear = (array, release_dateSelected) => {
+    return array.filter((item) => item.release_date === release_dateSelected);
+  };
+  
