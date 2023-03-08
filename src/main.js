e7b4c0b48e191e64dc1539340e2c2729192566b6
Carolina Vera <cvera.nahuelcoy@gmail.com>
2023-03-08 15:19:39 -0300

    función botones principales

--- src/main.js
+++ src/main.js
@@ -4,91 +4,119 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 // DOM
-//const btnHome = ;
+const btnHome = document.getElementById('buttonHome');
 const btnPokedex = document.getElementById('buttonPokedex');
-//const btnmap = document.getElementById ('buttonMap');
-//const btnextra = document.getElementById ('buttonExtra');
-/*const homeSecction = document.getElementById('homeSecction');
+const btnMap = document.getElementById('buttonMap');
+const btnExtra = document.getElementById('buttonExtra');
+const homeSecction = document.getElementById('homeSecction');
 const pokedexSecction = document.getElementById('pokedexSecction');
 const mapSecction = document.getElementById('mapSecction');
-const extraSecction = document.getElementById('extraSecction');*/
+const extraSecction = document.getElementById('extraSecction');
+
+
+function mostrarHome() {
+  homeSecction.style.display = 'block'
+  pokedexSecction.style.display = 'none'
+  mapSecction.style.display = 'none'
+  extraSecction.style.display = 'none'
+}
+
+function mostrarPokedex() {
+  pokedexSecction.style.display = 'block'
+  homeSecction.style.display = 'none'
+  mapSecction.style.display = 'none'
+  extraSecction.style.display = 'none'
+}
+
+function mostrarMap() {
+  mapSecction.style.display = 'block'
+  homeSecction.style.display = 'none'
+  pokedexSecction.style.display = 'none'
+  extraSecction.style.display = 'none'
+}
+
+function mostrarExtra() {
+  extraSecction.style.display = 'block'
+  homeSecction.style.display = 'none'
+  mapSecction.style.display = 'none'
+  pokedexSecction.style.display = 'none'
+}
+
+btnHome.addEventListener('click', mostrarHome);
+btnPokedex.addEventListener('click', mostrarPokedex);
+btnMap.addEventListener('click', mostrarMap);
+btnExtra.addEventListener('click', mostrarExtra);
 
 //mostrar seccion pokedex
-function mostrarPokedex() {
-    document.getElementById('homeSecction').style.display = 'none';
-    document.getElementById('mapSecction').style.display = 'none';
-    document.getElementById('extraSecction').style.display = 'none';
-    document.getElementById('pokedexSecction').style.display = 'block';
+/*function mostrarPokedex() {
+  document.getElementById('homeSecction').style.display = 'none';
+  document.getElementById('mapSecction').style.display = 'none';
+  document.getElementById('extraSecction').style.display = 'none';
+  document.getElementById('pokedexSecction').style.display = 'block';
 }
 
 document.getElementById('buttonPokedex').addEventListener('click', function () {
-    mostrarPokedex();
+  mostrarPokedex();
 })
 //mostras seccion home
 function mostrarHome() {
-    document.getElementById('homeSecction').style.display = 'block';
-    document.getElementById('mapSecction').style.display = 'none';
-    document.getElementById('extraSecction').style.display = 'none';
-    document.getElementById('pokedexSecction').style.display = 'none';
+  document.getElementById('homeSecction').style.display = 'block';
+  document.getElementById('mapSecction').style.display = 'none';
+  document.getElementById('extraSecction').style.display = 'none';
+  document.getElementById('pokedexSecction').style.display = 'none';
 }
 
 document.getElementById('buttonHome').addEventListener('click', function () {
-    mostrarHome();
+  mostrarHome();
 })
 
 //mostrar seccion mapa
 function mostrarMap() {
-    document.getElementById('homeSecction').style.display = 'none';
-    document.getElementById('mapSecction').style.display = 'block';
-    document.getElementById('extraSecction').style.display = 'none';
-    document.getElementById('pokedexSecction').style.display = 'none';
+  document.getElementById('homeSecction').style.display = 'none';
+  document.getElementById('mapSecction').style.display = 'block';
+  document.getElementById('extraSecction').style.display = 'none';
+  document.getElementById('pokedexSecction').style.display = 'none';
 }
 
 document.getElementById('buttonMap').addEventListener('click', function () {
-    mostrarMap();
+  mostrarMap();
 })
 
 //mostrar seccion Noticias
 function mostrarExtra() {
-    document.getElementById('homeSecction').style.display = 'none';
-    document.getElementById('mapSecction').style.display = 'none';
-    document.getElementById('extraSecction').style.display = 'block';
-    document.getElementById('pokedexSecction').style.display = 'none';
+  document.getElementById('homeSecction').style.display = 'none';
+  document.getElementById('mapSecction').style.display = 'none';
+  document.getElementById('extraSecction').style.display = 'block';
+  document.getElementById('pokedexSecction').style.display = 'none';
 }
 
 document.getElementById('buttonExtra').addEventListener('click', function () {
-    mostrarExtra();
+  mostrarExtra();
 })
 
 
+/*function mostrarSecction() {
+  if (homeSecction) {
+    homeSecction.style.display = 'block'
+    pokedexSecction.style.display = 'none'
+    mapSecction.style.display = 'none'
+    extraSecction.style.display = 'none'
+  } else if (pokedexSecction) {
+    pokedexSecction.style.display = 'block'
+    homeSecction.style.display = 'none'
+    mapSecction.style.display = 'none'
+    extraSecction.style.display = 'none'
+  } else if (mapSecction) {
+    mapSecction.style.display = 'block'
+    homeSecction.style.display = 'none'
+    pokedexSecction.style.display = 'none'
+    extraSecction.style.display = 'none'
+  } else  {
+    extraSecction.style.display = 'block'
+    homeSecction.style.display = 'none'
+    pokedexSecction.style.display = 'none'
+    mapSecction.style.display = 'none'
+  }
+}*/
 
-
-
-
-
-
-/*function mostrarSeccionHome() {
-    if (mostrarHome) {
-        homeSecction.style.display = 'block';
-    } else {
-        pokedexSection.style.display = 'none';
-        mapSecction.style.display = 'none';
-        extraSecction.style.display = 'none';
-    }
-},
-
-function mostrarSeccionPokedex() {
-    if (mostarPokedex) {
-        pokedexSecction.style.display = 'block'
-    } else {
-        homeSecction.style.display = 'none';
-        mapSecction.style.display = 'none';
-        extraSecction.style.display = 'none';
-
-    }
-}
-
-
-btnHome.addEventListener('click', mostrarSecctionHome)
-btnPokedex.addEventListener('click', mostrarSecctionPokedex)*/
-console.log(example, data);
+console.log(example);
