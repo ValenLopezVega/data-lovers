a57d25fca89f8bdda3b5419e4e60a0f7fe7cd41e
Jhoana <jhoanazamora2@gmail.com>
2023-03-10 16:08:20 -0500

    Estructuracion de vista secundaria

--- src/data.js
+++ src/data.js
@@ -1,9 +1,6 @@
 export const filterByDirector = (array, directorSelected) => {
   return array.filter((item) => item.director === directorSelected);
 };
-export const filterByProducer = (array, producerSelected) => {
-  return array.filter((item) => item.producer === producerSelected);
-};
 export const orderAzByTitle = (array) => {
   return array.sort((a, b) => a.title.localeCompare(b.title));
 };
@@ -12,8 +9,4 @@ export const orderZaByTitle = (array) => {
 };
 export const searchByTitle = (array, text) => {
   return array.filter((item) => item.title.toLowerCase().includes(text));
-};
-
-export const filterByYear = (array, release_dateSelected) => {
-  return array.filter((item) => item.release_date === release_dateSelected);
 };
