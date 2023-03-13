f06732f2382516673713be99486b5520c0809b15
abrakel <abravokeller@gmail.com>
2023-03-13 15:09:21 -0300

    Creacion de las cards de peliculas con formato en CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
+/* import { example } from './data/ghibli/ghibli.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+// import data from './data/rickandmorty/rickandmorty.js'; */
 
-console.log(example, data);
+fetch("data/ghibli/ghibli.json")
+.then (response => response.json())
+.then(data => {
+    crearTarjetas(data.films)})
+    .catch(error => console.log(error));
+
+    /*Cards Peliculas*/
+function crearTarjetas(peliculas){
+    let container = document.querySelector("section");
+    peliculas.forEach(pelicula => 
+        container.innerHTML += `
+        <div class="card">
+            <div class="card-body-img>
+                <img class="poster" src="${pelicula.poster}" alt="Imagen de la película"/>
+            </div>
+            <div class="card-body-text">
+                    <h5 class="card-title">${pelicula.title}</h5>
+                    <span class="info">${pelicula.release_date}</span>
+            </div>
+        </div>
+        `
+)
+console.log(peliculas)
+};
+
+// console.log(example, data);
