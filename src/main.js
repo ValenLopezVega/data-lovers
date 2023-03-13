87c6eeacab600610d7121cfe1cb122ac55addfb6
fiorella-yumi <fiorella.dm2002@gmail.com>
2023-03-13 17:13:13 -0500

    Visualizacion de la data en el interfaz

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { example } from './data.js';
 import {anotherExample} from './data.js';
 
 import data from './data/pokemon/pokemon.js';
-console.log(example,anotherExample,data);
+/*console.log(example,anotherExample,data);*/
 
 //DOM elements
-const targets = document.querySelectorAll('[data-target]')
-const content = document.querySelectorAll('[data-content]')
+const targets = document.querySelectorAll('[data-target]');
+const content = document.querySelectorAll('[data-content]');
+
+
 
 //Functions that interact with the DOM
 //Dynamic windows
@@ -20,3 +22,24 @@ targets.forEach(target => {
     t.classList.add('active');
   })
 });
+
+//data POkemon
+const datosPokedex = data.pokemon;
+function mostrar (dataArray) {
+ // const cotenedorPokedex =document.querySelector('.contenedor-pokedex')
+  let contentPokedex = "";
+  dataArray.forEach((elemento)=>{
+   // console.log('cada elemento; ', elemento)
+    contentPokedex += `<article> 
+                        <img src="${elemento.img}">
+                        <p>${elemento.num}</p>
+                        <p>${elemento.name}</p>
+                      </article>
+                        `
+    console.log('contenido pokedex' +contentPokedex);
+    })
+    console.log(dataArray);
+  document.getElementById('content-pokedexs').innerHTML = contentPokedex
+}
+mostrar (datosPokedex)
+
