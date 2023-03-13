8db0562f031ad30f58abc8edc86a735611d3e5b2
Fiorella Diaz <fiorella.dm2002@gmail.com>
2023-03-13 10:03:59 -0500

    Menu responsive con funcionalidad de ventanas

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import {anotherExample} from './data.js';
 import data from './data/pokemon/pokemon.js';
 console.log(example,anotherExample,data);
 
-//Elementos DOM
-const targets = document.querySelectorAll('[data-target]');
-const content = document.querySelectorAll('[data-content]');
- 
-//Funciones que interactuen con el DOM
+//DOM elements
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
 
+//Functions that interact with the DOM
+//Dynamic windows
 targets.forEach(target => {
   target.addEventListener('click', () => {
     content.forEach(c => {
