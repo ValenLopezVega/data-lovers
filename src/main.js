e460d2fb7242905867efbf2ed80fb9562ea3d904
abrakel <abravokeller@gmail.com>
2023-03-15 14:56:51 -0300

    dar formato al card para que no pierda sus dimensiones al cambiar de tamaño la pantalla y centrar elementos de section

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* import { example } from './data/ghibli/ghibli.js';
-// import data from './data/lol/lol.js';
+// import data from './data/lol/lol.js';*/
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js'; */
+console.log(data);
 
 fetch("data/ghibli/ghibli.json")
 .then (response => response.json())
@@ -15,17 +15,20 @@ function crearTarjetas(peliculas){
     peliculas.forEach(pelicula => 
         container.innerHTML += `
         <div class="card">
-            <div class="card-body-img>
+            <div class="card-body-img">
                 <img class="poster" src="${pelicula.poster}" alt="Imagen de la película"/>
             </div>
             <div class="card-body-text">
-                    <h5 class="card-title">${pelicula.title}</h5>
-                    <span class="info">${pelicula.release_date}</span>
+                <h5 class="card-title">${pelicula.title}</h5>
+                <span class="info">Año: ${pelicula.release_date}</span>
+                <span class="info">Score: ${pelicula.rt_score}</span>
             </div>
         </div>
         `
-)
+);
 console.log(peliculas)
 };
+
+
 
 // console.log(example, data);
