7f0cd2e34910d787bf369307843dead6a30ab87f
FRANCISCG97 <f.carrasco.g.97@gmail.com>
2023-03-16 17:18:55 -0300

    Commit prueba/cambios mostrar data en Interfaz

--- src/data.js
+++ src/data.js
@@ -1,67 +1,148 @@
 import data from "./data/ghibli/ghibli.js";
 
-//FILTROS
+console.log(data);
 
-//Director
-export const filtroDirector = (films, nombreDirector) => {
-  const returnDirector = films.filter((y) => y.director === nombreDirector);
-  return returnDirector;
-};
+//import { filtroDirector, filtroProductor, filtroEspecie, filtroGenero } from "./data.js";
 
-//Productor
-export const filtroProductor = (films, nombreProductor) => {
-  const returnProductor = films.filter((y) => y.producer === nombreProductor);
-  return returnProductor;
-};
+//MANIPULACION A TRAVES DE ARRAYS Y OBJETOS
+//FUNCIONALIDAD PARA MANIPULAR, OBTENER Y PROCESAR DATOS
+//FUNCIONES PURAS E INDEPENDIENTES DEL DOM
 
-//Especie
-export const arrayEspeciePersonajes = [];
-for (let i = 0; i < data.films.length; i++) {
-  for (let j = 0; j < data.films[i].people.length; j++) {
-    arrayEspeciePersonajes.push(data.films[i].people[j]);
-  }
-}
-export const filtroEspecie = (arrayEspeciePersonajes, especie) => {
-  const returnEspecie = arrayEspeciePersonajes.filter((y) => y.specie === especie);
-  return returnEspecie;
-};
+// const titulos = [data.films.title];
 
-//Género
-export const arrayGeneroPersonajes = [];
-for (let i = 0; i < data.films.length; i++) {
-  for (let j = 0; j < data.films[i].people.length; j++) {
-    arrayGeneroPersonajes.push(data.films[i].people[j]);
-  }
-}
-export const filtroGenero = (arrayGeneroPersonajes, genero) => {
-  const returnGenero = arrayGeneroPersonajes.filter((y) => y.gender === genero);
-  return returnGenero;
-};
+// for (const title of titulos) {
+//   title;
+//   console.log(title);}
 
-//ORDENAMIENTO
+//const result = data.filter(data => data.title );
 
-//Titulo
-export const tituloAscendente = (a, b) => {
-  if (a.title > b.title) {
-    return 1;
-  } else {
-    return -1;
-  }
-};
-export const tituloDescendente = (a, b) => {
-  if (a.title > b.title) {
-    return -1;
-  } else {
-    return 1;
-  }
-};
-export const ordenamientoTitulo = (order, films) => {
-  if (order === "ascendente") {
-    const returnOrdenamientoTitulo = films.sort(tituloAscendente);
-    return returnOrdenamientoTitulo;
-  }
-  else {
-    const returnOrdenamientoTitulo = films.sort(tituloDescendente);
-    return returnOrdenamientoTitulo;
-  }
-};
+// const titulos = data.films.map(function (data, title){
+// // tu iterador
+
+// });
+
+// const ordenado = data.films
+//titulos.forEach(console.log(titulos));
+//ordenado.forEach(element => console.log(element));
+// ordenado.sort();
+
+
+// for(let i = 0; i < data.films; i++) {
+//   data[i].infoType.forEach(function(item) {
+//     console.log(item[0]);
+//   })
+// }
+
+
+// const resultado = [];
+// for(let i = 0, len = data.films.title; i < len; i++){
+//   resultado.push(data.films[i][2])
+//   console.log(resultado)
+// }
+
+
+
+
+//ORDEN
+
+// function orderTitle(films) {
+
+// }
+
+
+
+// const titulos = [data.films.title];
+// for (let i = 0; i < titulos.length; i++) {
+//   console.log(titulos[i])}
+
+// Object.entries(titulos).forEach(([titulos, title]) => {
+//   console.log(titulos + ' ' + title); // "a 5", "b 7", "c 9"
+// });
+
+// const index = 0;
+// const titulos2 = titulos.map(function callback(films, index, title) {
+//   console.log(titulos2)// Elemento devuelto de nuevo_array
+// })
+
+//titulos.forEach(console.log(titulos));
+//titulos.forEach(data.films = console.log(titulos));
+// ordenado.sort();
+
+
+// titulos.sort();
+// console.log(titulos);
+
+// //TITULO
+// const tituloAscendente = (a, b) => {
+  
+//   for (let i = 0; i < titulos.length; i++) {
+//   // console.log(data.films[i].title);
+//   }
+  
+//   if (a.title > b.title) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// };
+// console.log(titulos);
+
+// export const tituloDescendente = (a, b) => {
+//   if (a.title > b.title) {
+//     return -1;
+//   } else {
+//     return 1;
+//   }
+// };
+// export const ordenamientoTitulo = (order, films) => {
+//   if (order === "ascendente") {
+//     const returnOrdenamientoTitulo = films.sort(tituloAscendente);
+//     return returnOrdenamientoTitulo;
+//   }
+//   else {
+//     const returnOrdenamientoTitulo = films.sort(tituloDescendente);
+//     return returnOrdenamientoTitulo;
+//   }
+// };
+
+
+
+
+// //FILTROS
+
+// //Director
+// export const filtroDirector = (data, director) => {
+//   const returnDirector = data.filter((y) => y.director === director);
+//   return returnDirector;
+// };
+// //document.getElementById("returnDirector").innerHTML = data.films(data, director);
+
+// //Productor
+// export const filtroProductor = (films, nombreProductor) => {
+//   const returnProductor = films.filter((y) => y.producer === nombreProductor);
+//   return returnProductor;
+// };
+
+// //Especie
+// export const arrayEspeciePersonajes = [];
+// for (let i = 0; i < data.films.length; i++) {
+//   for (let j = 0; j < data.films[i].people.length; j++) {
+//     arrayEspeciePersonajes.push(data.films[i].people[j]);
+//   }
+// }
+// export const filtroEspecie = (arrayEspeciePersonajes, especie) => {
+//   const returnEspecie = arrayEspeciePersonajes.filter((y) => y.specie === especie);
+//   return returnEspecie;
+// };
+
+// //Género
+// export const arrayGeneroPersonajes = [];
+// for (let i = 0; i < data.films.length; i++) {
+//   for (let j = 0; j < data.films[i].people.length; j++) {
+//     arrayGeneroPersonajes.push(data.films[i].people[j]);
+//   }
+// }
+// export const filtroGenero = (arrayGeneroPersonajes, genero) => {
+//   const returnGenero = arrayGeneroPersonajes.filter((y) => y.gender === genero);
+//   return returnGenero;
+// };
--- src/main.js
+++ src/main.js
@@ -1,24 +1,112 @@
 import data from './data/ghibli/ghibli.js';
-import { filtroDirector, filtroProductor, filtroEspecie, filtroGenero } from "./data.js";
+
+const peliculas= document.querySelector('#peliculas')
+
+//INTERACCION DEL DOM (LISTENERS, HANDLERS)
+//MUESTRA DATOS EN PANTALLA
 
 // SELECCION SECCION
+//HISTORIA
 const botonHistoria = document.getElementById("botonHistoria");
 const historia = document.getElementById("historia");
-botonHistoria.addEventListener("click", function() {
+const volver = document.getElementById("volver");
+
+botonHistoria.addEventListener("click", function () {
   document.getElementById("header").style.display = "none";
   historia.style.display = "block";
 });
 
+function refresh() {
+  window.location.reload();
+}
+
+//VOLVER
+
+volver.addEventListener("click", refresh);
+
+
+
+
+//PERSONAJES
 const botonPersonajes = document.getElementById("botonPersonajes");
 const seccionPersonajes = document.getElementById("personajes");
-botonPersonajes.addEventListener("click", function() {
+const volver2 = document.getElementById("volver2");
+
+
+botonPersonajes.addEventListener("click", function () {
   document.getElementById("header").style.display = "none";
   seccionPersonajes.style.display = "block";
 });
 
+function refresh2() {
+  window.location.reload();
+}
+
+//DISABLE
+const especieBtn = document.getElementById('especie');
+const generoBtn = document.getElementById('genero')
+let generoDsbd = false
+let especieDsbd = false
+
+
+generoBtn.addEventListener("click", function () {
+  especieDsbd = !especieDsbd;
+  especieBtn.disabled = especieDsbd;
+});
+
+especieBtn.addEventListener("click", function () {
+  generoDsbd = !generoDsbd;
+  generoBtn.disabled = generoDsbd;
+});
+
+//VOLVER
+
+volver2.addEventListener("click", refresh2);
+
+
+
+
+//FILMOGRAFIA
 const botonFilmografia = document.getElementById("botonFilmografia");
 const seccionFilmografia = document.getElementById("filmografia");
-botonFilmografia.addEventListener("click", function() {
+const volver3 = document.getElementById("volver3");
+
+botonFilmografia.addEventListener("click", function () {
   document.getElementById("header").style.display = "none";
   seccionFilmografia.style.display = "block";
 });
+
+// const down = document.getElementById("descendente");
+
+// function bajar() {
+// }
+// down.addEventListener("click", bajar)
+
+// document.getElementById("descendente").innerHTML = down;
+
+
+
+
+function refresh3() {
+  window.location.reload();
+}
+
+//VOLVER
+
+volver3.addEventListener("click", refresh3);
+
+function createHtml(pelicula) {
+  const titulo = `<h2>La pelicula es:${pelicula}</h2>`;
+  return titulo
+}
+
+function printData(films){
+  let htmlAcumulado=''
+  films.forEach(function(pelicula) {
+    htmlAcumulado+=createHtml(pelicula.title);
+  })
+  peliculas.innerHTML=htmlAcumulado;
+}
+printData(data.films)
+
+console.log(data.films[2].title);
