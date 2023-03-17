e1e99b61ce99bc19d353c9c4addf755638396082
andrenavas <andreynanavas@gmail.com>
2023-03-17 14:08:02 -0500

    Add: functions on data and new readme

--- src/data.js
+++ src/data.js
@@ -1,9 +1,33 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const searchByName = (data,name) => {
+  return data.filter((characters) => {
+    return characters.name.toLowerCase().includes(name.toLowerCase());
+  })
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filterBySpecies = (data,species) => {
+  return data.filter((characters) => {
+    return characters.species.includes(species);
+  })
 };
+
+export const filterByGender = (data,gender) => {
+  return data.filter((characters) => {
+    return characters.gender.includes(gender);
+  })
+};
+
+export const filterByOrderAZ = (characters) => {
+  return characters.sort((a,b) => {
+    if (a.name < b.name) {
+      return -1;
+    } 
+  })
+};
+
+export const filterByOrderZA = (characters) => {
+  return characters.sort((a,b) => {
+    if (a.name > b.name) {
+      return -1;
+    }
+  })
+}
