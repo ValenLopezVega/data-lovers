05ed6255ed9efe1d9a6b18206bce1ff798b36f3c
andrenavas <andreynanavas@gmail.com>
2023-03-17 14:28:46 -0500

    Add: info back card in main, test for functions and images readme

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,117 @@
-import { example, anotherExample } from '../src/data.js';
+import { searchByName,filterBySpecies, filterByGender, filterByOrderAZ, filterByOrderZA} from '../src/data.js';
+//import rickandmorty from '../src/data/rickandmorty/rickandmorty.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+//Función que filtra por nombre
+describe('searchByName', () => {
+  it('Debería ser una función', () => {
+    expect(typeof searchByName).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it ('debería devolver un array vacío si el resultado no coincide', () => {
+    const characters = [
+      { name: 'Rick Sanchez', species: 'Human' },
+      { name: 'Morty Smith', species: 'Human' }
+    ];
+
+    const results = searchByName(characters,'Beth');
+    expect (results).toEqual([]);
+  });
+
+  it ('debería devolver el personaje que el usuario está buscando', () => {
+    const characters = [
+      { name: 'Rick Sanchez', species: 'Human'},
+      { name: 'Morty Smith', species: 'Human' }
+    ];
+
+    const results = searchByName(characters,'rick');
+    expect (results).toEqual([{ name: 'Rick Sanchez', species: 'Human' }]);
+  });
+
+  it ('debería poder buscar sin importar si el usuario ingresa mayúsculas o minúsculas', () => {
+    const characters = [
+      { name: 'Rick Sanchez', species: 'Human'},
+      { name: 'Morty Smith', species: 'Human' }
+    ];
+
+    const results = searchByName(characters,'RICK');
+    expect (results).toEqual([{ name: 'Rick Sanchez', species: 'Human' }]);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+//Función que filtra por especie
+describe('filterBySpecies', () => {
+  it('Debería ser una función', () => {
+    expect(typeof filterBySpecies).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it ('debería devolver todos los robots cuando el usuario seleccione la opción robots', () => {
+    const characters = [
+      { name: 'Rick Sanchez', species: 'Human'},
+      { name: 'Morty Smith', species: 'Human' },
+      {name: 'Conroy', species: "Robot"},
+    ];
+
+    const results = filterBySpecies(characters,'Robot');
+    expect (results).toEqual([{name: 'Conroy', species: "Robot"}]);
+  });
+});
+
+//Función que filtra por género
+describe('filterByGender', () => {
+  it('Debería ser una función', () => {
+    expect(typeof filterByGender).toBe('function');
+  });
+
+  it ('debería devolver todos los personajes femeninos cuando el usuario seleccione la opción femenino', () => {
+    const characters = [
+      { name: 'Rick Sanchez', gender: 'Male'},
+      { name: 'Morty Smith', gender: 'Male' },
+      { name: 'Creepy Little Girl', gender: 'Female'},
+      { name: 'Cynthia', gender: 'Female'}
+    ];
+
+    const results = filterByGender(characters,'Female');
+    expect (results).toEqual([ {name: 'Creepy Little Girl', gender: 'Female'},{name: 'Cynthia', gender: 'Female'}]);
+  });
+});
+
+// Función que ordena de la A a la Z
+
+describe('filterByOrderAZ', () => {
+  it('Debería ser una función', () => {
+    expect(typeof filterByOrderAZ).toBe('function');
+  });
+
+  it ('debería devolver todos los personajes ordenados alfabéticamente de formas ascendente', () => {
+    const characters = [
+      { name: 'Rick Sanchez', gender: 'Male'},
+      { name: 'Morty Smith', gender: 'Male' },
+      { name: 'Creepy Little Girl', gender: 'Female'},
+      { name: 'Cynthia', gender: 'Female'}
+    ];
+
+    const results = filterByOrderAZ(characters);
+    expect (results).toEqual([ {name: 'Creepy Little Girl', gender: 'Female'},{name: 'Cynthia', gender: 'Female'},{ name: 'Morty Smith', gender: 'Male' },{ name: 'Rick Sanchez', gender: 'Male'} ]);
+  });
+});
+
+// Función que ordena de la Z a la A
+
+describe('filterByOrderZA', () => {
+  it('Debería ser una función', () => {
+    expect(typeof filterByOrderZA).toBe('function');
+  });
+
+  it ('debería devolver todos los personajes ordenados alfabéticamente de formas ascendente', () => {
+    const characters = [
+      { name: 'Rick Sanchez', gender: 'Male'},
+      { name: 'Morty Smith', gender: 'Male' },
+      { name: 'Creepy Little Girl', gender: 'Female'},
+      { name: 'Cynthia', gender: 'Female'}
+    ];
+
+    const results = filterByOrderZA(characters);
+    expect (results).toEqual([ { name: 'Rick Sanchez', gender: 'Male'}, { name: 'Morty Smith', gender: 'Male' }, {name: 'Cynthia', gender: 'Female'}, {name: 'Creepy Little Girl', gender: 'Female'}]);
   });
 });
