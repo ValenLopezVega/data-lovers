b5f1fdb1731cd045e7ef5a32f1ee7112d6d835e3
abrakel <abravokeller@gmail.com>
2023-03-20 09:30:55 -0300

    Se añadio la funcion filtrar por año y se adapto para celular

--- src/data.js
+++ src/data.js
@@ -1,24 +1,13 @@
-// estas funciones son de ejemplo
 
-fetch("data/ghibli/ghibli.json")
-.then (response => response.json())
-.then(data => {
-    myJson(data.films)})
-
-let arrayMovie = JSON.parse(myJson);
-console.log(arrayMovie);
-
-
-const movieArray = json.films;
-console.log(movieArray)
-
-let filterMovie = arrayMovie.filter(movie => movie.release_date === release_date);
-console.log(filterMovie);
 
 export const example = () => {
-  return 'example';
+  return 'array de Score';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export function filterMovies(peliculas, years) {
+  var newArray = peliculas.filter(function (año) {
+    return año.release_date == years;
+  });
+    return newArray;
 };
+
--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
-/* import { example } from './data/ghibli/ghibli.js';
-// import data from './data/lol/lol.js';*/
+import { filterMovies  } from './data.js';
 import data from './data/ghibli/ghibli.js';
-console.log(data);
-
-fetch("data/ghibli/ghibli.json")
-.then (response => response.json())
-.then(data => {
-    crearTarjetas(data.films)})
-    .catch(error => console.log(error));
-
+    
     /*Cards Peliculas*/
 function crearTarjetas(peliculas){
     let container = document.querySelector("section");
-    peliculas.forEach(pelicula => 
+    container.innerHTML = "";
+     peliculas.forEach(pelicula => 
         container.innerHTML += `
         <div class="card">
             <div class="card-body-img">
@@ -20,15 +13,42 @@ function crearTarjetas(peliculas){
             </div>
             <div class="card-body-text">
                 <h5 class="card-title">${pelicula.title}</h5>
-                <span class="info">Año: ${pelicula.release_date}</span>
-                <span class="info">Score: ${pelicula.rt_score}</span>
+                <span class="año">Año: ${pelicula.release_date}</span>
+                <span class="score">Score: ${pelicula.rt_score}</span>
+                <a href="#">Ver más</a>
+                </div>
+            </div>
+        `
+     );
+    }
+const filterElement = document.querySelector(".select-filter");
+const filterYear = document.querySelector(".selection-option");
+crearTarjetas(data.films);
+
+    filterYear.addEventListener("change", (filter) => {
+    if (filter.target.value === "all"){
+        console.log("todas las peliculas")
+        return crearTarjetas(data.films);
+
+    } else {
+        return crearTarjetas(filterMovies(data.films, filter.target.value));
+        };  
+    });
+
+const masInfo = document.getElementsByTagName("a")
+crearTarjetas(data.films);
+masInfo.addEventListener("click", (description) => 
+description.forEach(personaje => 
+    container.innerHTML +=`
+        <div class="card">
+            <div class="card-body-img">
+                <img class="poster" src="${personaje.people.img[2]}" alt="Imagen de la película"/>
+            </div>
+            <div class="card-body-text">
+                <h5 class="card-title">${personaje.people.name[1]}</h5>
+                <span class="año">Edad: ${personaje.people.age[4]}</span>
+                <span class="score">Especie: ${personaje.people.specie[7]}</span>
             </div>
         </div>
         `
-);
-console.log(peliculas)
-};
-
-
-
-// console.log(example, data);
+));
