934b58cbabf8df56253d5618fcba965b2ef5c748
_AngieMora1 <angiemorarosas@gmail.com>
2023-03-20 12:16:31 -0500

    Filtrado en la interfaz por tipos

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
-
 export function typeFilter (arrData,n) {
   const typeFilterData = arrData.filter((elem) => elem.type.includes(n));
   return typeFilterData;
 }
-
+  
 export const anotherExample = () => {
   return 'OMG';
 }
