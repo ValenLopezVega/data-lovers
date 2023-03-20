8b893a657a128574bec54660e2ee3676a35ba172
NatSama2 <vet.nat.c@gmail.com>
2023-03-20 16:00:23 -0300

    empezando test

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
-// estas funciones son de ejemplo
-
+// Funcion filtrar por tipo
 
 export const pokemonData = (data,condition) => {  
   const filterType= data.filter(pokemon => pokemon.type === condition);
   return filterType;
 };
 
-
+//funcion orenar data
 export const sortPokemon = (pokemones, order)=>{
   const copia = [...pokemones]
-return copia.sort((a, b) => order === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
+  return copia.sort((a, b) => order === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
 };
 
+// funcion calcular el spawn min y max
 export const calculateSpawnMinMax = (data) => {
   const spawnChanceArray = data.pokemon
     .map(pokemon => parseFloat(pokemon['spawn-chance']))
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,17 +1,71 @@
-import { example, anotherExample } from '../src/data.js';
+import { pokemonData, sortPokemon, calculateSpawnMinMax } from '../src/data/pokemon/pokemon.js';
 
-
-describe('example', () => {
+//test tipo
+describe('pokemonData', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof pokemonData).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns an array of filtered pokemons', () => {
+    const data = [
+      { name: 'Bulbasaur', type: 'Grass' },
+      { name: 'Charmander', type: 'Fire' },
+      { name: 'Squirtle', type: 'Water' },
+      { name: 'Pikachu', type: 'electric' },
+    ];
+    const condition = 'Fire';
+
+    expect(pokemonData(data, condition)).toEqual([{ name: 'Charmander', type: 'Fire' }]);
+  });
+
+  it('returns true for {name: "Bulbasaur", type: "Grass"}', () => {
+    const pokemon = { name: 'Bulbasaur', type: 'Grass' };
+    expect(pokemonData([pokemon], 'Grass')).toContainEqual(pokemon);
   });
 });
 
+//test por filtro ordenar 
+describe('sortPokemon', () => {
+  it('is a function', () => {
+    expect(typeof sortPokemon).toBe('function');
+  });
 
+  it('returns true for Asc', () => {
+    expect(copia.sort((a, b) => order === "asc")).toBe('true');
+  });
+
+it('returns true for Desc', () => {
+  expect(order === "Desc"(a, b)).toBe('true');
+});
+}); 
+
+//test calculateSpawnMinMax
+describe('calculateSpawnMinMax', () => {
+  it('is a function', () => {
+    expect(typeof calculateSpawnMinMax).toBe('function');
+  });
+
+  it('returns `minSpawnChance`', () => {
+    expect(Math.min(...spawnChanceArray)).toBe('true');
+  });
+
+  it('returns `MaxSpawnChance`', () => {
+    expect(Math.max(...spawnChanceArray)).toBe('true');
+  });
+});
+  /*
+export const calculateSpawnMinMax = (data) => {
+  const spawnChanceArray = data.pokemon
+    .map(pokemon => parseFloat(pokemon['spawn-chance']))
+    .filter(spawnChance => typeof spawnChance === 'number' && !isNaN(spawnChance));
+  const minSpawnChance = Math.min(...spawnChanceArray);
+  const maxSpawnChance = Math.max(...spawnChanceArray);
+
+  return {min: minSpawnChance, max: maxSpawnChance};
+}
+*/
+
+/*
 describe('anotherExample', () => {
   it('is a function', () => {
     expect(typeof anotherExample).toBe('function');
@@ -19,5 +73,4 @@ describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
-  });
-});
+  }); */
