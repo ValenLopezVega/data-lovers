684ee165f14374c65e0a89bcb4e42f7832464790
Brina <brixiga1892@gmail.com>
2023-03-21 09:59:19 -0500

    agregando funcionalidades

--- src/data.js
+++ src/data.js
@@ -7,3 +7,25 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+export const cartillas = (data) => {
+  let cartillaString = "";
+  for (const pokemon of data.pokemon) {
+    cartillaString += `
+    <article>
+    <h3>${pokemon.name}</h3>
+    <p>
+    ${pokemon.about}
+    </p>
+    <img src=${pokemon.img}>
+    </article>`;
+  }
+  return cartillaString;
+};
+
+
+export const buscarPorNombre = (data, nombreBuscar) => {
+  const busca = nombreBuscar.trim();
+  const dataFiltrada = data.pokemon.filter((pokemon) => pokemon.name.startsWith(busca))
+  return { pokemon: dataFiltrada }
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
-import { example } from './data.js';
+//import { example } from './data.js';
+//import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
+
+import { buscarPorNombre, cartillas} from "./data.js";
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-const generation= pokemon.filter(region => (region.generation .length===2));
-prompt (generation);
-console.log(example, data);
+//console.log(example, data);
+
+const inputBuscar = document.getElementById("inputBuscar");
+const listadepokemon =data.pokemon;
+
+const pokeSection= document.getElementById("seccionUno");
+
+inputBuscar.addEventListener("input", function () {
+  const FiltroNombre = buscarPorNombre(data, inputBuscar.value.trim().substring(0, 3));
+  if (FiltroNombre.pokemon.length === 0) {
+    alert("verica el nombre del pokemon ingresado");
+  }
+  pokeSection.innerHTML = cartillas(FiltroNombre);
+});
+
+listadepokemon.forEach((item)=>{
+  const pokeCard = document.createElement("article");
+  const poketitulo = document.createElement("h3");
+  const pokeParrafo = document.createElement("p");
+  const pokeImagen = document.createElement("img");
+
+  poketitulo.textContent = item.name;
+  pokeParrafo.textContent = item.about;
+  pokeImagen.src= item.img;
+  
+  pokeSection.appendChild(pokeCard);
+  pokeCard.appendChild(poketitulo);
+  pokeCard.appendChild(pokeParrafo);
+  pokeCard.appendChild(pokeImagen);
+} );
+
