2c36f7cbc5f142cc0f5ce76661f0761ce0e882f8
Fiorella Diaz <fiorella.dm2002@gmail.com>
2023-03-21 11:46:47 -0500

    Funcion filtrado por tipos y por generacion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { typeFilter } from './data.js';
-import {anotherExample} from './data.js';
+import {typeFilterGeneration} from './data.js';
 import data from './data/pokemon/pokemon.js';
 
 
@@ -30,10 +30,13 @@ function mostrar (dataArray) {
   document.getElementById('content-pokedexs').innerHTML = contentPokedexMain
 }
 mostrar (dataPokedex)
+
 //Option select filter
 const selFilter = document.getElementById("type-pokemon");
 selFilter.addEventListener("change",() => {
   const infoFiltrado=selFilter.selectedIndex;
+  console.log(infoFiltrado)
+
   if(infoFiltrado === 0){
     console.log(typeFilter(dataPokedex,"grass"))
     return mostrar(typeFilter(dataPokedex,"grass"))
@@ -47,10 +50,63 @@ selFilter.addEventListener("change",() => {
     console.log(typeFilter(dataPokedex,"flying"))
     return mostrar(typeFilter(dataPokedex,"flying"))
   }else if(infoFiltrado === 4){
-    console.log(typeFilter(dataPokedex,"water"))
-    return mostrar(typeFilter(dataPokedex,"water"))
+    return mostrar(typeFilter(dataPokedex, "water"))
+  } else if(infoFiltrado === 5){
+    console.log((typeFilter(dataPokedex, "bug")))
+    return mostrar(typeFilter(dataPokedex, "bug"))
+  } else if(infoFiltrado === 6){
+    console.log(typeFilter(dataPokedex, "normal"))
+    return mostrar(typeFilter(dataPokedex, "normal"))
+  } else if(infoFiltrado === 7){
+    console.log(typeFilter(dataPokedex, "electric"))
+    return mostrar(typeFilter(dataPokedex, "electric"))
+  } else if(infoFiltrado === 8){
+    console.log(typeFilter(dataPokedex, "ground"))
+    return mostrar(typeFilter(dataPokedex, "ground"))
+  } else if(infoFiltrado === 9){
+    console.log(typeFilter(dataPokedex, "fighting"))
+    return mostrar(typeFilter(dataPokedex, "fighting"))
+  } else if(infoFiltrado === 10){
+    console.log(typeFilter(dataPokedex, "psychic"))
+    return mostrar(typeFilter(dataPokedex, "psychic"))
+  } else if(infoFiltrado === 11){
+    console.log(typeFilter(dataPokedex, "rock"))
+    return mostrar(typeFilter(dataPokedex, "rock"))
+  } else if(infoFiltrado === 12){
+    console.log(typeFilter(dataPokedex, "ice"))
+    return mostrar(typeFilter(dataPokedex, "ice"))
+  } else if(infoFiltrado === 13){
+    console.log(typeFilter(dataPokedex, "ghost"))
+    return mostrar(typeFilter(dataPokedex, "ghost"))
+  } else if(infoFiltrado === 14){
+    console.log(typeFilter(dataPokedex, "dragon"))
+    return mostrar(typeFilter(dataPokedex, "dragon"))
+  } else if(infoFiltrado === 15){
+    console.log(typeFilter(dataPokedex, "dark"))
+    return mostrar(typeFilter(dataPokedex, "dark"))
+  } else if(infoFiltrado === 16){
+    console.log(typeFilter(dataPokedex, "dark"))
+    return mostrar(typeFilter(dataPokedex, "dark"))
+  } else if(infoFiltrado === 17){
+    console.log(typeFilter(dataPokedex, "steel"))
+    return mostrar(typeFilter(dataPokedex, "steel"))
   }
 })
+
+const selectGeneration = document.getElementById("type-generation");
+
+selectGeneration.addEventListener("change",() => {
+  const filterNumberTwo = selectGeneration.selectedIndex;
+  console.log(filterNumberTwo);
+  if (filterNumberTwo === 0) {
+    console.log(typeFilterGeneration(dataPokedex, "kanto"))
+    return mostrar(typeFilterGeneration(dataPokedex, "kanto"))
+  } else if(filterNumberTwo === 1){
+    console.log(typeFilterGeneration(dataPokedex, "johto"))
+    return mostrar(typeFilterGeneration(dataPokedex, "johto"))
+  }
+})
+
 
 
 
