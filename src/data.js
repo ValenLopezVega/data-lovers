9e6671f6a5d24041c911f76e71f0ae031459028a
unknown <caroraquelq@gmail.com>
2023-03-22 18:14:35 -0500

    Actualizar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,48 @@
 // estas funciones son de ejemplo
+import dataFunction from './data/ghibli/ghibli.js';
 
 export const example = () => {
   return 'example';
 };
 
+
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+
+
+
+
+/*const functions = {
+  printPoster: ( ) => 
+    dataFunction.films.forEach((item) => {
+      const post = item.poster;
+      const title = item.title;
+      const insertPoster = document.getElementById('insertPoster');
+      insertPoster.innerHTML += `<div class="imgPoster"> <img src=${post} alt="${title}"></img> <p>${title}</p</div>`
+    })
+  }*/
+
+  
+
+
+ 
+//console.log(functions.printPoster( ));
+
+ export const printPoster= ()=>{
+  dataFunction.films.forEach((item) => {
+    const post = item.poster;
+    const title = item.title;
+    const insertPoster = document.getElementById('insertPoster');
+    insertPoster.innerHTML += `<div class="imgPoster"> <img src=${post} alt="${title}"></img> <p>${title}</p</div>`;
+  })
+   
+}
+
+  
+ 
+ 
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { example } from './data.js';
+import { printPoster } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+
+printPoster();
+
 
 console.log(data);
 
@@ -11,39 +14,4 @@ console.log(data);
 //Recorrer la data, ubicar propiedad FILMS => un array => al elemento de array=>acceder al propiedad POSTER
 //Cada valor de las propiedades POSTER, se almacene en un div 
 //
-
-console.log("prueba 1", data.films);
-
-const practice = data.films[3].poster;
-
-
-const practice1 = data.films[4].poster;
-const practice2 = data.films[5].poster;
-const practice3 = data.films[6].poster;
-
-console.log("prueba 2",practice);
-console.log("prueba 3",practice1);
-console.log("prueba 4",practice2);
-console.log("prueba 5",practice3);
-
-
-
-
-
-//for (i=0; )
-
-
-
-const prueba = data.films;
-
-prueba.forEach((item)=>{
-   //console.log(item);
-    const post=item.poster;
-    const title=item.title;
-    const insertPoster=document.getElementById('insertPoster');
-
-    console.log(post);
-    insertPoster.innerHTML +=`<div class="imgPoster"> <img src=${post} alt="${title}"></img> <p>${title}</p</div>`
-    
-
-})
+ 
