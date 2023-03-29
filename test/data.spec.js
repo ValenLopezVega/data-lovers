7325f4da768c66f7bad42aaf57d085f20db26ea5
_AngieMora1 <angiemorarosas@gmail.com>
2023-03-29 00:55:17 -0500

    CSS tabla de estadisticas

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,34 @@
 import { filter } from '../src/data.js';
 
+const dataPoke = [
+  {
+    num: "001",
+    name: "bulbasaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.69",
+  },
+  {
+    num: "002",
+    name: "ivysaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.042",
+  },
+  {
+    num: "172",
+    name: "pichu",
+    generation: {
+      name: "johto",
+    },
+    type: ["electric"],
+    "spawn-chance": "0",
+  },
+];
 
 describe('filter', () => {
   it('is a object', () => {
@@ -10,9 +39,24 @@ describe('filter', () => {
       expect(typeof filter.typeFilter).toBe('function');
     })
   })
-  //it('returns `example`', () => {
-  //expect(example()).toBe('example');
-  //});
+  it('el filtro debe retornar un array con los pokemones de tipo veneno', () => {
+    const type = "poison"
+    const filteredPoke = filter.typeFilter(dataPoke,type);
+    
+    // por cada pokemon en filterdPoke 
+    // verificar que su atributo type(array) contenga el string 'poison'
+  
+    expect(filteredPoke.length).toBe(2)
+    expect(filteredPoke[0].type.includes(type)).toBe(true)
+    expect(filteredPoke[1].type.includes(type)).toBe(true)
+
+    filteredPoke.forEach(poke => {
+      expect(poke.type.includes(type)).toBe(true)
+    })  
+
+    // console.log(filteredPoke)
+    // console.log(filteredPoke[0])
+  });
 });
 
 
