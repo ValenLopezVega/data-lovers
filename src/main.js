f0ebfda6f80c2cd661500bdabcc9cdf95a393a19
unknown <caroraquelq@gmail.com>
2023-03-29 12:31:49 -0500

    Actualización de filtrado y ordenamiento

--- src/main.js
+++ src/main.js
@@ -1,17 +1,89 @@
-import { printPoster } from './data.js';
+import { filterByProducer, orderAlphabetical_AZ, orderAlphabetical_ZA } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-printPoster();
+//variable de data de peliculas en array
+
+//console.log(arrayFilms);
+
+const arrayFilms = data.films;
+//IMPRIMIR POSTER DE TODAS LAS PELICULAS
+const insertPoster = document.getElementById('insertPoster');
+
+function printAllPoster(allFilms) {
+
+    allFilms.forEach((item) => {
+        const post = item.poster;
+        const title = item.title;
+        insertPoster.innerHTML += `<div class="imgPoster"> <img src=${post} alt="${title}"></img> <p>${title}</p</div>`;
+
+    })
+
+}
 
 
-console.log(data);
+//DECLARANDO FUNCION DE IMPRIMIR
+printAllPoster(arrayFilms)
 
-// Crear un espacio (div) en HTML para insertar la data
-// Filtrar, traer la data "poster" en un constante o función
-// Insertar en el div de HTML.
-//Recorrer la data, ubicar propiedad FILMS => un array => al elemento de array=>acceder al propiedad POSTER
-//Cada valor de las propiedades POSTER, se almacene en un div 
-//
- 
+const selectProducer = document.getElementById('selectProducer')
+
+
+//CAPTURAR EL EVENTO
+//PODER IGUALAR EL EVENTO(VALUE) CON EL PARAMETRO DEL FILTRO(OPTION)
+
+
+selectProducer.addEventListener('change', selectOption);
+
+
+//console.log('prueba', )
+
+function selectOption() {
+    insertPoster.innerHTML = '';
+    let optionValue = selectProducer.value;
+    console.log('prueba1', optionValue);
+   
+    const filter = filterByProducer(arrayFilms, optionValue);
+    printAllPoster(filter)
+    console.log(filter)
+  
+    if (optionValue === 'all') {
+        insertPoster.innerHTML = '';
+        printAllPoster(arrayFilms)
+    }
+}
+
+
+
+const alphabeticalOrder = document.getElementById('alphabeticalOrder')
+alphabeticalOrder.addEventListener('change', orderOption);
+
+function orderOption() {
+    let orderValue = alphabeticalOrder.value;
+    console.log('prueba1', orderValue);
+
+    if (orderValue === 'A-Z') {
+        insertPoster.innerHTML = '';
+        const orderA_Z = orderAlphabetical_AZ(arrayFilms);
+        console.log('prueba2', orderA_Z)
+        printAllPoster(orderA_Z)
+    }
+    else {
+        insertPoster.innerHTML = '';
+        const orderZ_A = orderAlphabetical_ZA(arrayFilms);
+        console.log('prueba3', orderZ_A)
+        printAllPoster(orderZ_A)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
