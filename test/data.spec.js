2a5a06cf3b3e675c322a941f07ad016ae0c052fb
abrakel <abravokeller@gmail.com>
2023-03-30 15:44:25 -0300

    Se hicieron correcciones a los test y se terminaron

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,52 +1,71 @@
-import { filterMovies, filtrarPro, sortData, sortNum } from '../src/data.js';
-import data from '../src/data/ghibli/ghibli.json';
+import { filterMovies, filtrarPro, sortData , sortNum } from '../src/data.js';
+import { datatitle } from './mockData.js';
 
-describe('filterMovies', () => {
-  it('Debería ser una función', () => {
+describe('Probar si filterMovies es una función', () => {
+  it('filterMovies debería ser una función', () => {
     expect(typeof filterMovies ).toBe('function');
   });
 
   it('Debería retornar "Castle in the Sky" para "1986"', () => {
-    expect(filterMovies(data.films,'1986')).toContainEqual(data.films[0]);
+    expect(filterMovies(datatitle.films,"1986")).toContainEqual(datatitle.films[0]);
   });
 
   it('Debería retornar "From Up on Poppy Hill" para "2011"', () => {
-    expect(filterMovies(data.films,'2011')).toContainEqual(data.films[16]);
+    expect(filterMovies(datatitle.films,'1988')).toContainEqual(datatitle.films[1]);
   });
 
 });
 
-describe('filtrarPro', () => {
-  it('Debería ser una función', () => {
+describe('Probar si filtrarPro es una función', () => {
+  it('filtrarPro debería ser una función', () => {
     expect(typeof filtrarPro).toBe('function');
   });
 
-  it('Debería retornar ${data.films[16].release_date} para "1986"', () => {
-    expect(filtrarPro(data.films, 'Toru Hara')).toContainEqual(data.films[3]);
+  it('Debe retornar "Castle in the Sky" para "Isao Takahata"', () => {
+    expect(filtrarPro(datatitle.films, 'Isao Takahata')).toContainEqual(datatitle.films[0]);
+  });
+
+  it('Debería retornar "My Neighbor Totoro" para "Hayao Miyazaki"', () => {
+    expect(filtrarPro(datatitle.films, 'Hayao Miyazaki')).toContainEqual(datatitle.films[1]);
   });
 });
 
-describe('sortData', () => {
-  it('Debería ser una función', () => {
+describe('Probar que sortData permite ordenar las películas alfabéticamente', () => {
+
+  it('sortData es una función', () => {
     expect(typeof sortData).toBe('function');
   });
 
-  it('Debería retornar peliculas de A-Z', () => {
-    expect(sortData(data.films)).toContainEqual(data.films[0, 16, 3, 12, 2, 1, 9, 4, 6, 14, 5, 8, 10, 13, 11, 15, 18, 17, 19, 7]);
-  });
-});
+  it('Debe ordenar las películas alfabéticamente', () => {
+    const ordenAlfabetico = sortData(datatitle.films)
 
-describe('sortNum', () => {
-  it('Debería ser una función', () => {
-    expect(typeof sortNum).toBe('function');
-  });
-
-  it('Debería retornar peliculas ordenadas por año', () => {
-    expect(sortNum(data.films, 'año')).toContainEqual(data.films[0, 3, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 17, 19]);
+    expect(ordenAlfabetico[0].title).toMatch("Castle in the Sky");
+    expect(ordenAlfabetico[1].title).toMatch("Kiki's Delivery Service");
+    expect(ordenAlfabetico[2].title).toMatch("My Neighbor Totoro");
+    expect(ordenAlfabetico[3].title).toMatch("My Neighbor Totoro");
   });
 
-  it('Debería retornar peliculas ordenadas por score', () => {
-    expect(sortNum(data.films, 'score')).toContainEqual(data.films[13, 9, 6, 16, 12, 11, 17, 7, 14, 8, 19, 1, 5, 0, 15, 2, 3, 10, 4, 18]);
+  it('Debería retornar igual cuando los títulos se repitan', () => {
+    expect(sortData(datatitle.films)).toEqual(datatitle.films);
   });
-  
-});
+
+  describe('Probar que sortNum permite ordenar las películas según el año', () => {
+    it('sortNum debería ser una función', () => {
+      expect(typeof sortNum).toBe('function');
+    });
+
+    it('Debe ordenar las películas de menor a mayor año y puntaje', () => {
+      expect(sortNum(datatitle.films, 'año')).toContainEqual(datatitle.films[0,1,2,3]);
+
+      expect(sortNum(datatitle.films, 'score')).toContainEqual(datatitle.films[1,2,0,3]);
+    });
+
+    it ('Debería ordenar por puntaje', () => {
+      expect(sortNum(datatitle.films, 'puntaje')).toEqual(datatitle.films);
+    })
+
+    it ('Debería ordenar por año', () => {
+      expect(sortNum(datatitle.films, 'año')).toEqual(datatitle.films);
+    })
+  })
+})
