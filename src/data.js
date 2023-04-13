1846ca13cb22419e4b7aff226b930a22b80d2259
Maria T <flormaria_2424@outlook.com>
2023-04-13 18:30:24 -0500

    ULTIMOS AJUSTES DEL CODIGO - solo ubicar de mejor forma el codigo

--- src/data.js
+++ src/data.js
@@ -22,6 +22,22 @@ export const ordenar = {
   }
 }
 
+//te devuelve un string que contiene la estructura html , la cual se forma apartir
+// de la lista recibida por parametro
+export const cartillasHtml = (listaPokemons) => {
+  let cartillaString = ""; 
+  listaPokemons.forEach ((pokemon)=>{
+    cartillaString = cartillaString + `
+    <article class = "cartillaSeccUno">
+    <h3>${pokemon.name}</h3> 
+    <p>${pokemon.about}</p>
+    <img class= "pokecart" src=${pokemon.img}>
+    <a href="pokemon.html?nombre=${pokemon.name}" target="_blank" >see more</a>
+    </article>`;
+  });
+  return cartillaString;
+}
+
 function compararNombresAscendente( a, b ) {
   if ( a.name < b.name ){
     return -1;
@@ -42,18 +58,3 @@ function compararNombresDescendente( a, b ) {
   return 0;
 }
 
-//te devuelve un string que contiene la estructura html , la cual se forma apartir
-// de la lista recibida por parametro
-export const cartillasHtml = (listaPokemons) => {
-  let cartillaString = ""; 
-  listaPokemons.forEach ((pokemon)=>{
-    cartillaString = cartillaString + `
-    <article class = "cartillaSeccUno">
-      <h3>${pokemon.name}</h3>
-      <p>${pokemon.about}</p>
-      <img class= "pokecart" src=${pokemon.img}>
-      <a href="pokemon.html?nombre=${pokemon.name}" target="_blank" >see more</a>
-    </article>`;
-  });
-  return cartillaString;
-}
