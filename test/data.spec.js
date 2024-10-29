fd12bb0f7ca4c2629cdebf0e912937d9cf0bd673
valen2004vega <valen2004vega@gmail.com>
2024-10-29 16:03:06 -0500

    feat: new test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,47 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterData, sortData, computeStats } from '../src/data.js';
+import data from '../src/data/ghibli/ghibli.js';
 
-
-describe('example', () => {
+describe('filterData', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterData).toBe('function');
   });
 
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    const result = filterData(data, 'castle');
+    expect(result.length).toBe(2);
   });
 });
 
 
-describe('anotherExample', () => {
+describe('sortData', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof sortData).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns the second movie order ascendent', () => {
+    const result = sortData(data, 'title', 'asc')
+    expect(result[1].title).toBe('from up on poppy hill');
+  });
+
+  it('returns the first movie order descendent', () => {
+    const result = sortData(data, 'title', 'desc')
+    expect(result[0].title).toBe('whisper of the heart');
+  });
+});
+
+
+describe('computeStats', () => {
+  it('is a function', () => {
+    expect(typeof computeStats).toBe('function');
+  });
+
+  it('returns object length', () => {
+    const result = computeStats(data);
+    expect(Object.keys(result).length).toBe(5);
+  });
+
+  it('returns object length', () => {
+    const result = computeStats(data);
+    expect(Object.keys(result).length).toBe(5);
   });
 });
